1a17f4bce35552326744294a9e23789c8527c5aa
Jeong-Taeho <nabi218218@gmail.com>
2023-08-12 01:43:41 +0900

    style : tailwindcss 추가

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{html,js,ts,vue,tsx,jsx}"],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
