fa44c1230a7b9f076b1f21f95445f7022bb9f352
Jeong-Taeho <nabi218218@gmail.com>
2023-08-12 01:44:57 +0900

    feat : 컴포넌트 추가

---
+++ src/components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="text-white w-full font-bold text-xl mb-5">
+    <slot></slot>
+  </button>
+</template>
---
+++ src/components/Loading.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+    <span class="text-3xl font-bold text-white">...Loading</span>
+  </div>
+</template>
