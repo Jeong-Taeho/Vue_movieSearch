7ced76bf1e61ce63426dd7bf6542fb89b36c9b6f
Jeong-Taeho <nabi218218@gmail.com>
2023-08-12 01:45:24 +0900

    style : tailwindcss 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import router from "~/routes";
+import router from "./routes";
 import App from "~/App.vue";
+import "~/index.css";
 
 createApp(App).use(createPinia()).use(router).mount("#app");
